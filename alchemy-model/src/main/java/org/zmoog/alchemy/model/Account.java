edf405b8a9d58c29e4014e651a441a4866ff669b
Maurizio Branca <maurizio.branca@gmail.com>
2013-05-17 01:29:03 +0200

    Simple Spring MVC controller for the application Restful API

--- alchemy-model/src/main/java/org/zmoog/alchemy/model/Account.java
+++ alchemy-model/src/main/java/org/zmoog/alchemy/model/Account.java
@@ -1,3 +1,68 @@
 package org.zmoog.alchemy.model;
 
-public class Account {}
+import java.util.Date;
+
+public class Account {
+	
+	String id;
+	
+	String name;
+	
+	String description;
+	
+	Date createdAt;
+	
+	String createdBy;
+
+	// Constructors
+	
+	public Account() {
+	}
+
+	public Account(String name) {
+		this.name = name;
+	}
+
+	// Accessors 
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+}
